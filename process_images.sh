9f1a639613e6267d2063859d77f597c64c481b27
Zhiyuan Pan <zy_pan@zju.edu.cn>
2024-11-11 01:52:50 +0800

    upload

--- process_images.sh
+++ process_images.sh
@@ -2,9 +2,6 @@ gulp rename-images
 gulp resize-images
 gulp delete
 
-git checkout --orphan tmp
 git add -A
 git commit -m "upload"
-git branch -D master
-git branch -m master
 git push -f origin master
